3b5e592917e9b4749870b24991ddf14b3a522663
lolymarsh <marshloly@gmail.com>
2024-01-21 14:02:08 +0700

    FIX

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,12 @@ import ResumeComponent from "./Components/ResumeComponent";
 const App = () => {
   return (
     <ContainerComponent>
-      <div className="container">
-        <ResumeComponent text="Resume" />
-        <H2Component text="นายภูวดล ทนค้ำ" />
-        <PComponent text="สีที่ชอบ สีเขียว" />
-        <PComponent text="ความสูง 175 ซม." />
-        <PComponent text="น้ำหนัก 60 กก." />
-        <PComponent text="คำคมที่ชอบ ความสุขไม่ได้มาจากการมีสิ่งของมากมาย แต่มาจากการรับรู้และดีใจกับสิ่งที่มี" />
-      </div>
+      <ResumeComponent text="Resume" />
+      <H2Component text="นายภูวดล ทนค้ำ" />
+      <PComponent text="สีที่ชอบ สีเขียว" />
+      <PComponent text="ความสูง 175 ซม." />
+      <PComponent text="น้ำหนัก 60 กก." />
+      <PComponent text="คำคมที่ชอบ ความสุขไม่ได้มาจากการมีสิ่งของมากมาย แต่มาจากการรับรู้และดีใจกับสิ่งที่มี" />
     </ContainerComponent>
   );
 };
